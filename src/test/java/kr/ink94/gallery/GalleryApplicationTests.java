6c03f2744e351e54b381362c602845f968e7a191
인경수 <yinks94@gmail.com>
2025-04-30 16:24:07 +0900

    초기화

---
+++ src/test/java/kr/ink94/gallery/GalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ink94.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
